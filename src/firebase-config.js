dd0604fdc66b0c1b04a29eb73cc82576cb2b83df
AlexMan123456 <alex.man03888@gmail.com>
2025-02-19 02:17:23 +0000

    Put Firebase credentials in environment variable, loading them in with import.meta.env

--- src/firebase-config.js
+++ src/firebase-config.js
@@ -8,14 +8,13 @@ import { getStorage } from "firebase/storage"
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyA2oloCkw25JQ9DHL_-J3xUOCA1q3gU6vU",
-  authDomain: "neurosongs-file-storage.firebaseapp.com",
-  projectId: "neurosongs-file-storage",
-  storageBucket: "neurosongs-file-storage.firebasestorage.app",
-  messagingSenderId: "258711973688",
-  appId: "1:258711973688:web:aa352789964fb927119bcd",
-  measurementId: "G-2S989ZS972",
-  storageBucket: "neurosongs-file-storage.firebasestorage.app"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
