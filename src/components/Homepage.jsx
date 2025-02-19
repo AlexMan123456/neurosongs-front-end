cdeb713930f8d77f28e8f234c18ffd46661c4485
AlexMan123456 <alex.man03888@gmail.com>
2025-02-19 12:07:50 +0000

    Edited homepage, will add featured songs later

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -1,33 +1,5 @@
-import { useEffect, useState } from "react";
-import { getAllSongs } from "../../api";
-import SongCard from "./songs/SongCard";
-import SongList from "./songs/SongList";
-
 function Homepage(){
-    const [songs, setSongs] = useState([]);
-    const [isLoading, setIsLoading] = useState(true);
-    const [error, setError] = useState("")
-
-    useEffect(() => {
-        setIsLoading(true);
-        getAllSongs().then((songs) => {
-            setIsLoading(false);
-            setSongs(songs);
-        }).catch((err) => {
-            setIsLoading(false);
-            setError("Could not fetch songs. Please try again later.");
-        })
-    }, [])
-
-    if(isLoading){
-        return <p>Now Loading...</p>
-    }
-
-    if(error){
-        return <p>{error}</p>
-    }
-
-    return (<SongList songs={songs}/>)
+    return (<h2>Welcome to Neurosongs!</h2>)
 }
 
 export default Homepage
