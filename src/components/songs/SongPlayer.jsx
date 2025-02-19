b4037c2b7d059d9dd56d37fdf891214aac43f7e7
AlexMan123456 <alex.man03888@gmail.com>
2025-02-19 17:08:50 +0000

    Added more info to song page

--- src/components/songs/SongPlayer.jsx
+++ src/components/songs/SongPlayer.jsx
@@ -5,16 +5,19 @@ import { getDownloadURL, ref } from "firebase/storage";
 import { storage } from "../../firebase-config";
 import 'react-h5-audio-player/lib/styles.css';
 import H5AudioPlayer from "react-h5-audio-player";
+import StyledLink from "../styling/StyledLink";
 
 function SongPlayer(){
     const {song_id} = useParams()
     const [song, setSong] = useState(null);
+    const [songData, setSongData] = useState({})
     const [isLoading, setIsLoading] = useState(true);
     const [error, setError] = useState("")
 
     useEffect(() => {
         setIsLoading(true);
         getSong(song_id).then((songData) => {
+            setSongData(songData)
             const songRef = ref(storage, `${songData.username}/${songData.album_id}/${songData.reference}`);
             return getDownloadURL(songRef);
         }).then((songURL) => {
@@ -34,7 +37,11 @@ function SongPlayer(){
         return <p>{error}</p>
     }
 
-    return <H5AudioPlayer src={song}/>
+    return (<section>
+        <h2>{songData.title}</h2>
+        <StyledLink to={`/users/${songData.username}`}>{songData.artist.artist_name}</StyledLink>
+        <H5AudioPlayer src={song}/>
+    </section>)
 }
 
 export default SongPlayer
