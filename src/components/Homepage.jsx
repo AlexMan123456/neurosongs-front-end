9dc77799eaad12e9dfdc66c59dff648fa1cf93d3
AlexMan123456 <alex.man03888@gmail.com>
2025-02-19 18:27:19 +0000

    Optimised loading homepage

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -4,36 +4,27 @@ import SongCard from "./songs/SongCard";
 
 function Homepage(){
     const [featuredSongs, setFeaturedSongs] = useState([]);
-    const [isLoading, setIsLoading] = useState(true);
-    const [error, setError] = useState("");
+    const [error, setError] = useState("")
 
     useEffect(() => {
-        setIsLoading(true);
         getSongs({is_featured: true}).then((songs) => {
-            setIsLoading(false);
             setFeaturedSongs(songs);
         }).catch((err) => {
-            setIsLoading(false);
             setError("Usually we'd display a list of featured songs, but we're having problems at the moment...");
         })
     }, [])
 
     return (<section>
         <h2>Welcome to Neurosongs!</h2>
-        {isLoading ? <p>Now Loading...</p> : null}
-        {error 
-        ? 
+        {error ? <p>{error}</p> : 
         <>
-            <h2>This is embarassing...</h2>
-            <p>{error}</p>
-        </>
-        : null}
-        <h3>Featured Songs</h3>
-        <ol>
-            {featuredSongs.map((song) => {
-                return (<SongCard song={song}/>)
-            })}
-        </ol>
+            <h3>Featured Songs</h3>
+            <ol>
+                {featuredSongs.map((song) => {
+                    return (<SongCard song={song}/>)
+                })}
+            </ol>
+        </>}
     </section>)
 }
 
