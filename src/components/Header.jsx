89318fb694d027ea994f382a5c7f5e0f6d96f9db
AlexMan123456 <alex.man03888@gmail.com>
2025-02-19 12:06:13 +0000

    Added nav bar to header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,9 @@
 import Navigation from "./Navigation"
+import StyledLink from "./styling/StyledLink"
 
 function Header(){
     return (<header>
+        <StyledLink to="/">Homepage</StyledLink>
         <h1>Neurosongs</h1>
         <Navigation/>
     </header>)
