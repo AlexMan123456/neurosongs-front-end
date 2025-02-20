46dbc7049a8d6acfda86c897266f8e3d7e484899
AlexMan123456 <alex.man03888@gmail.com>
2025-02-20 12:41:41 +0000

    Made some styling changes to album page

--- src/components/albums/AlbumPage.jsx
+++ src/components/albums/AlbumPage.jsx
@@ -6,6 +6,7 @@ import AlbumSongCard from "./AlbumSongCard";
 import StyledLink from "../styling/StyledLink";
 import H5AudioPlayer from "react-h5-audio-player";
 import AlbumSongPlayer from "./AlbumSongPlayer";
+import NowPlaying from "./NowPlaying";
 
 function AlbumPage(){
     const {album_id} = useParams();
@@ -38,7 +39,13 @@ function AlbumPage(){
     return (<section>
         <h2>{album.title}</h2>
         <p>By <StyledLink to={`/users/${album.username}`}>{album.artist.artist_name}</StyledLink></p>
-        {song_id ? <AlbumSongPlayer album={album}/> : null}
+        
+        {song_id
+        ? <div>
+            <NowPlaying/>    
+            <AlbumSongPlayer album={album}/>
+        </div>
+        : null}
         <ol>
             {album.songs.map((song) => {
                 return <AlbumSongCard key={`song-card-${song.song_id}`} song={song}/>
---
+++ src/components/albums/NowPlaying.jsx
@@ -0,0 +1,37 @@
+import { useEffect, useState } from "react";
+import { useSearchParams } from "react-router-dom"
+import { getSongById } from "../../../api";
+
+function NowPlaying(){
+    const [searchParams, setSearchParams] = useSearchParams();
+    const song_id = searchParams.get("song_id");
+    const [songData, setSongData] = useState([])
+    const [isLoading, setIsLoading] = useState(true);
+    const [error, setError] = useState("");
+
+    useEffect(() => {
+        setIsLoading(true);
+        getSongById(song_id).then((song) => {
+            setIsLoading(false);
+            setSongData(song);
+        }).catch((err) => {
+            setIsLoading(false);
+            setError("Could not get song metadata. Please try again later.")
+        })
+    }, [])
+
+    if(isLoading){
+        return <p>Now Loading...</p>
+    }
+
+    if(error){
+        return <p>{error}</p>
+    }
+
+    return (<>
+        <h3>Now Playing</h3>
+        <p>{songData.title} - {songData.artist.artist_name}</p>
+    </>)
+}
+
+export default NowPlaying
