94d1509e3d3d30a69590c9abc339fdf425e6436f
AlexMan123456 <alex.man03888@gmail.com>
2025-02-21 21:11:27 +0000

    Added user data to profile page

--- src/components/users/UserPage.jsx
+++ src/components/users/UserPage.jsx
@@ -1,21 +1,31 @@
-import { useEffect, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import { useParams } from "react-router-dom"
 import { getUserById } from "../../../api";
 import UserSongs from "./UserSongs";
 import Loading from "../Loading";
 import StyledLink from "../styling/StyledLink";
+import { Avatar } from "@mui/material";
+import { getDownloadURL, ref } from "firebase/storage";
+import { storage } from "../../firebase-config";
+import { UserContext } from "../../contexts/UserContext";
 
 function UserPage(props){
     const {user_id} = useParams();
-    const [user, setUser] = useState({})
-    const [isLoading, setIsLoading] = useState(true)
-    const [error, setError] = useState("")
+    const [user, setUser] = useState({});
+    const [isLoading, setIsLoading] = useState(true);
+    const [error, setError] = useState("");
+    const [profilePictureURL, setProfilePictureURL] = useState(null);
+    const {signedInUser} = useContext(UserContext);
 
     useEffect(() => {
         setIsLoading(true)
         getUserById(user_id).then((user) => {
-            setIsLoading(false);
             setUser(user);
+            const profilePictureRef = ref(storage, `${user.user_id}/images/profile-picture/${user.profile_picture}`);
+            return getDownloadURL(profilePictureRef);
+        }).then((profilePictureURL) => {
+            setIsLoading(false);
+            setProfilePictureURL(profilePictureURL)
         }).catch((err) => {
             setIsLoading(false);
             setError("Could not fetch user. Please try again later.");
@@ -31,9 +41,15 @@ function UserPage(props){
     }
 
     return (<section>
+        <Avatar
+            src={profilePictureURL}
+            alt={`${user.username}'s profile picture`}
+        />
         <h2>{user.artist_name}</h2>
         <p>{`@${user.username}`}</p>
-        <StyledLink to={`/users/${user_id}/settings`}>User Settings</StyledLink>
+        <h2>Description</h2>
+        <p>{user.description}</p>
+        {signedInUser.user_id === user_id ? <StyledLink to={`/users/${user_id}/settings`}>User Settings</StyledLink> : null}
         <h3>Songs</h3>
         <UserSongs user_id={user_id}/>
     </section>)
