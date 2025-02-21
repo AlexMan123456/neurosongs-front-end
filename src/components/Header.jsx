e759a37421a64312e8553700d98d8f1c458aa8ae
AlexMan123456 <alex.man03888@gmail.com>
2025-02-21 14:13:40 +0000

    Added dropdown menu to header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,17 +1,49 @@
-import { useContext } from "react"
+import { useContext, useState } from "react"
 import Navigation from "./Navigation"
 import StyledLink from "./styling/StyledLink"
 import { UserContext } from "../contexts/UserContext"
+import { Avatar, List, ListItem, ListItemButton, ListItemText } from "@mui/material";
+import { useNavigate } from "react-router-dom";
 
 function Header(){
     const {signedInUser} = useContext(UserContext);
     const isUserSignedIn = Object.keys(signedInUser).length !== 0;
+    const [displayUserList, setDisplayUserList] = useState(false);
+    const navigate = useNavigate()
+
+    function handleViewProfile(){
+        setDisplayUserList(false);
+        navigate(`/users/${signedInUser.user_id}`)
+    }
+
     return (<header>
-        {isUserSignedIn ? <StyledLink to={`users/${signedInUser.user_id}`}>View Profile</StyledLink> : <StyledLink to={`/sign_in`}>Sign In</StyledLink>}
-        {isUserSignedIn ? <p>Signed in as {signedInUser.artist_name}</p> : null}
+        {isUserSignedIn
+        ?
+        <Avatar
+            src={signedInUser.profile_picture}
+            alt={`${signedInUser.username}'s profile picture`}
+            onClick={() => {setDisplayUserList((displayUserList) => {return !displayUserList})}}
+        />
+        :
+        <StyledLink to={`/sign_in`}>Sign In</StyledLink>}
+        {displayUserList ? 
+            <List>
+                <ListItem>
+                    <ListItemButton onClick={handleViewProfile}>
+                        <ListItemText primary="View Profile"/>
+                    </ListItemButton>
+                </ListItem>
+                <ListItem>
+                    <ListItemButton>
+                        <ListItemText primary="Sign Out"/>
+                    </ListItemButton>
+                </ListItem>
+            </List>
+        : null}
         <h1>Neurosongs</h1>
         <Navigation/>
     </header>)
 }
+//<StyledLink to={`users/${signedInUser.user_id}`}>View Profile</StyledLink>
 
 export default Header
