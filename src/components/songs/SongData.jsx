a963dac50122b16dd62e09b8ee3c496c6f13d186
AlexMan123456 <alex.man03888@gmail.com>
2025-02-24 00:12:06 +0000

    Added link to albums on song page

--- src/components/songs/SongData.jsx
+++ src/components/songs/SongData.jsx
@@ -20,6 +20,7 @@ function SongData({song, songData, frontCover, backCover}){
         {songData.album.back_cover_reference ? <Button onClick={() => {setDisplayFront((displayFront) => {return !displayFront})}}>View {displayFront ? "back" : "front"} cover</Button> : null}
         <h2>{songData.title}</h2>
         <p>{songData.artist.artist_name} (<StyledLink to={`/users/${songData.user_id}`}>@{songData.artist.username}</StyledLink>)</p>
+        <p>Album: <StyledLink to={`/albums/${songData.album_id}`}>{songData.album.title}</StyledLink></p>
         <h3>Description</h3>
         {songData.description.split("\n").map((paragraph, index) => {
             return <p key={`song-${songData.song_id}-paragraph-${index}`}>{paragraph}</p>
