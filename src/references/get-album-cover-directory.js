be49b1acdbd8dd53e371501d57b7e0f0502ed89e
AlexMan123456 <alex.man03888@gmail.com>
2025-02-25 16:51:38 +0000

    Added default front cover to album cover references

--- src/references/get-album-cover-directory.js
+++ src/references/get-album-cover-directory.js
@@ -1,4 +1,7 @@
 function getAlbumCoverDirectory(album, side){
+    if(album.front_cover_reference === "Default"){
+        return "default-front-album-cover.png"
+    }
     return `${album.user_id}/albums/${album.album_id}/images/${album[side === "front" ? "front_cover_reference" : "back_cover_reference"]}`
 }
 
