bf916cf531cdb14a151ec31a37f3100870ce9782
AlexMan123456 <alex.man03888@gmail.com>
2025-02-25 11:40:28 +0000

    Added a new image component with styling option for size

---
+++ src/components/styling/StyledImage.jsx
@@ -0,0 +1,10 @@
+function StyledImage({src, alt}){
+    return <img 
+        style={{
+            width: "15vw",
+            height: "auto"
+        }}
+        src={src}
+        alt={alt}
+    />
+}
