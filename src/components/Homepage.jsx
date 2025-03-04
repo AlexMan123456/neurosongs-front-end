dc09796674288c302654fa78cee6c5b4fe3e5f47
AlexMan123456 <alex.man03888@gmail.com>
2025-03-04 11:37:13 +0000

    Update homepage

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -34,13 +34,11 @@ function Homepage(){
             : null}
             <p>Welcome to my latest project. This is a website I created called Neurosongs, and it essentially aims to be a community site for musicians. You can think of it as being somewhat of a mix between YouTube and Spotify, in that it has the streaming capabilities of Spotify, mixed with the community aspects of YouTube.</p>
 
-            <p>You can either experience this site as a signed out user, or a signed in user. As a signed out user, you'll only be able to explore the songs and albums, and see other people's comments. It's a serviceable experience, but nothing special.</p>
-
-            <p>However, as a signed in user, you'll also have the ability to leave comments and ratings on other people's music, as well as post some of your own songs for others to hear! You can also change your profile picture, set a user description, and really make your presence on this site unique to you.</p>
+            <p>You can either experience this site as a signed out user, or a signed in user. As a signed out user, you'll only be able to explore the songs and albums, and see other people's comments. However, as a signed in user, you'll also have the ability to leave comments and ratings on other people's music, follow other users so that you can easily keep up-to-date with what they're posting, as well as post some of your own songs for others to hear! You can also change your profile picture, set a user description, and really make your presence on this site unique to you.</p>
 
             <p>Signing in is managed with the help of Firebase to help make the experience smooth and secure. You can either sign in with Google, or directly by email. If you sign in with email, you'll also need to provide a password, but don't worry - Firebase deals with this securely so I don't ever have access to it.</p>
 
-            <p>If you want to hear some of my own songs that I've created, you can check out the featured page. There you'll find an album I've called Neural Anthems++, which is a remake of an album I made back in 2023, as well as a few suggested songs from the album that I'm most proud of.</p>
+            <p>If you want to hear some of my own songs that I've created, you can check out my own profile, <StyledLink to="https://neurosongs.netlify.app/users/7asm4JCpeBXep2LwrcurUdrHpFO2">@AlexTheMan</StyledLink>. There you'll find a bunch of songs I've created with the help of Logic Pro X as my DAW, and Synth V for access to some vocal synths to sing the vocal melodies I've created.</p>
 
             <p>With all that said, have fun exploring the site! If you enjoyed your visit (and even if you didn't), please feel free to let me know your thoughts by filling out <StyledLink to="https://docs.google.com/forms/d/e/1FAIpQLScILg15VZOUU-JwXj9nh5amvmOP35VE_cGz4BIAMRzzA8Alkw/viewform?usp=dialog">this feedback form.</StyledLink> This will allow me to keep improving the experience and keep working to make this site the best it can be!</p>
 
