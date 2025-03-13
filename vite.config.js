d9260632034d9947b7db90831d6b2cdf0e7269bf
AlexMan123456 <alex.man03888@gmail.com>
2025-03-13 15:12:12 +0000

    Configure manifest.json file

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import { VitePWA } from 'vite-plugin-pwa'
+import manifest from "./manifest.json"
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -8,6 +9,7 @@ export default defineConfig({
     react(),
     VitePWA(
       {
+        ...manifest,
         registerType: "autoUpdate",
         devOptions: {
           enabled: true
