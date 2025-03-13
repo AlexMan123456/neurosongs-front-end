ea9269615ac79e661c7f5918bb39d76eeb31b361
AlexMan123456 <alex.man03888@gmail.com>
2025-03-13 19:16:55 +0000

    Changed the required dimensions to be considered a large screen

--- src/utils/is-screen-large.js
+++ src/utils/is-screen-large.js
@@ -1,5 +1,5 @@
 function isScreenLarge(window){
-    return window.innerWidth > 495 && window.innerHeight > 800
+    return window.innerWidth > 669 && window.innerHeight > 800
 }
 
 export default isScreenLarge
