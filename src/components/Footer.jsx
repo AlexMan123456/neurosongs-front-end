6e5e2b8d4fffd1ede89d0cf819b5174d35eb2929
AlexMan123456 <alex.man03888@gmail.com>
2025-03-22 17:06:23 +0000

    Removed donations link from footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -11,7 +11,8 @@ function Footer(){
         {showFooter ? <List>
             <li><ListItemButton component={Link} to="https://github.com/AlexMan123456/neurosongs-back-end">GitHub back-end repository</ListItemButton></li>
             <li><ListItemButton component={Link} to="https://github.com/AlexMan123456/neurosongs-front-end">GitHub front-end repository</ListItemButton></li>
-            <li><ListItemButton component={Link} to="/donations">Leave a donation (EXPERIMENTAL)</ListItemButton></li>
+            {//<li><ListItemButton component={Link} to="/donations">Leave a donation (EXPERIMENTAL)</ListItemButton></li>
+            }
         </List> : null}
     </footer>)
 }
