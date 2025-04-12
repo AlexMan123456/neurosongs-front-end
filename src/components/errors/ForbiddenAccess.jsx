cc8c18bef0a1c842b1dec3e7800c0766c379496b
AlexMan123456 <alex.man03888@gmail.com>
2025-04-12 01:49:19 +0100

    Add link back to homepage

--- src/components/errors/ForbiddenAccess.jsx
+++ src/components/errors/ForbiddenAccess.jsx
@@ -1,7 +1,10 @@
+import StyledLink from "#components/styling/StyledLink"
+
 function ForbiddenAccess(){
     return (<section>
         <h2>Access Denied!</h2>
         <p>You're not supposed to be here!</p>
+        <StyledLink to="/">Return to homepage</StyledLink>
     </section>)
 }
 
