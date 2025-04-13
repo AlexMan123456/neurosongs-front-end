ec8af2c326c6e32a03777f8f04599ad1a02a9d09
AlexMan123456 <alex.man03888@gmail.com>
2025-04-14 00:40:24 +0100

    Fixed issue with visibility header showing up when signed in user is not on their user page

--- src/components/songs/SongsTable.jsx
+++ src/components/songs/SongsTable.jsx
@@ -1,9 +1,13 @@
-import { useLocation } from "react-router-dom";
+import { useLocation, useParams } from "react-router-dom";
 import SongCard from "./SongCard";
 import { Table, TableCell, TableContainer, TableHead, TableRow } from "@mui/material";
+import { useContext } from "react";
+import { UserContext } from "#contexts/UserContext";
 
 function SongsTable({songs, setSongs}){
     const location = useLocation();
+    const {user_id} = useParams();
+    const {signedInUser} = useContext(UserContext)
 
     return (
         <TableContainer>
@@ -15,7 +19,7 @@ function SongsTable({songs, setSongs}){
                         <TableCell>Artist</TableCell>
                         {!location.pathname.includes("/users") ? <TableCell>Username</TableCell> : null}
                         <TableCell>Album</TableCell>
-                        <TableCell>Visibility</TableCell>
+                        {location.pathname.includes("/users") && signedInUser.user_id === user_id ? <TableCell>Visibility</TableCell> : null}
                     </TableRow>
                 </TableHead>
                 {songs.map((song) => {
